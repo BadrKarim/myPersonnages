7320f717330c83379d1d0716f50bcdcd41ee1ca3
BadrKarim <karim-badr@hotmail.fr>
2022-11-28 17:21:42 +0100

    controller

--- src/Entity/Arme.php
+++ src/Entity/Arme.php
@@ -17,6 +17,33 @@ class Arme{
         self::$armes[] = $this;
     }
 
+    public function getmon(){
+        return $this->nom;
+    }
+
+    public function getDescription(){
+        return $this->nom;
+    }
+
+    public function getDegat(){
+        return $this->nom;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function setDescription($Description){
+        $this->Description = $Description;
+        return $this;
+    }
+
+    public function setDegat($Degat){
+        $this->Degat = $Degat;
+        return $this;
+    }
+    
     public static function creerArmes(){
         $p1 = new Arme("épée", "Une superbe épée" ,25);
 
